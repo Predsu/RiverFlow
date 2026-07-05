f25c7a87cefaac4e40b10f17e7c3b5c42fb4ee26
ToriYukari <kuklaradek1@gmail.com>
2026-07-05 23:52:59 +0200

    Initial Commit

---
+++ RiverFlow/RiverFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiverFlowApp.swift
+//  RiverFlow
+//
+//  Created by ToriYukari on 05/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct RiverFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RiverFlowTests/RiverFlowTests.swift
@@ -0,0 +1,16 @@
+//
+//  RiverFlowTests.swift
+//  RiverFlowTests
+//
+//  Created by ToriYukari on 05/07/2026.
+//
+
+import Testing
+
+struct RiverFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
